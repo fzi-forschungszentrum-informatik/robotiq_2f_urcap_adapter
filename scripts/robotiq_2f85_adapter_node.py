da61ade177a539d1508b52241ab23026ad7c4f47
Felix Exner <exner@fzi.de>
2022-11-24 16:26:57 +0000

    Add ament_lint tests.
    Fix python style issues.
    Fix package.xml issues.

--- scripts/robotiq_2f85_adapter_node.py
+++ scripts/robotiq_2f85_adapter_node.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python3
-"""
-Module containing adapters for the Robotq_2f85 offering a direct interface between the gripper and ROS.
-"""
+"""Module containing adapters for the Robotq_2f85 offering direct gripper <-> ROS interface."""
 
 # Copyright (c) 2022 FZI Forschungszentrum Informatik
 #
@@ -35,7 +33,8 @@ from robotiq_2f85_urcap_adapter.action import MoveGripper
 
 
 class GripperStatus(Enum):
-    """Gripper status reported by the gripper. The integer values have to match what the gripper sends."""
+    """Gripper status reported by the gripper."""
+
     RESET = 0
     ACTIVATING = 1
     # UNUSED = 2  # This value is currently not used by the gripper firmware
@@ -43,7 +42,8 @@ class GripperStatus(Enum):
 
 
 class ObjectStatus(Enum):
-    """Object status reported by the gripper. The integer values have to match what the gripper sends."""
+    """Object status reported by the gripper."""
+
     MOVING = 0
     STOPPED_OUTER_OBJECT = 1
     STOPPED_INNER_OBJECT = 2
@@ -53,6 +53,7 @@ class ObjectStatus(Enum):
 def clip_val(min_value: int, value: int, max_value: int) -> int:
     """
     Clips a value to the bounds specified by both arguments.
+
     :param min_value: Lowest value returned
     :param value: Value that should be clipped to the bounds.
     :param max_value: Maximum value returned
@@ -63,9 +64,7 @@ def clip_val(min_value: int, value: int, max_value: int) -> int:
 
 
 class Robotiq2f85AdapterNode(Node):
-    """
-    ROS node offering an action server to control a Robotq2f85 gripper using URCAP commands over sockets.
-    """
+    """ROS node offering ROS actions to control a Robotq2f85 gripper using string commands."""
 
     def __init__(self):
         super().__init__("robotiq_2f86_urcap_adapter")
@@ -80,8 +79,8 @@ class Robotiq2f85AdapterNode(Node):
         self.declare_parameter("robot_port", 502)
         self.declare_parameter("publish_fake_joint_values", True)
 
-        robot_ip = self.get_parameter("robot_ip").value
-        robot_port = self.get_parameter("robot_port").value
+        robot_ip: str = self.get_parameter("robot_ip").value
+        robot_port: int = self.get_parameter("robot_port").value
 
         self.get_logger().info(f"Connecting to URCAP on {robot_ip}:{robot_port}!")
 
@@ -100,10 +99,8 @@ class Robotiq2f85AdapterNode(Node):
             self.timer = self.create_timer(self.timer_period, self.publish_joint_values)
 
     def disconnect(self):
-        """
-        Disconnect from the URCAP socket.
-        """
-        self.get_logger().info(f"Disconnecting from URCAP!")
+        """Disconnect from the URCAP socket."""
+        self.get_logger().info("Disconnecting from URCAP!")
         self.gripper_adapter.disconnect()
 
     def __move_gripper_to_position(self, goal_handle, position: int) -> MoveGripper.Result:
@@ -142,7 +139,7 @@ class Robotiq2f85AdapterNode(Node):
 
     def execute_callback(self, goal_handle):
         """
-        Callback used to move the robotiq gripper with ros actions.
+        Run the callback to move the robotiq gripper with ros actions.
 
         :param goal_handle: ROS action server goal handle.
         :return MoveGripper.Result containing the result of the execution.
@@ -167,9 +164,7 @@ class Robotiq2f85AdapterNode(Node):
         )
 
     def publish_joint_values(self):
-        """
-        Publishes the current joint values of the finger_joint at regular intervals.
-        """
+        """Publish the current joint values of the finger_joint at regular intervals."""
         msg = JointState()
         msg.header.stamp = self.get_clock().now().to_msg()
         msg.name = ["finger_joint"]
@@ -180,32 +175,27 @@ class Robotiq2f85AdapterNode(Node):
 
 
 class Robotiq2f85SocketAdapter:
-    """
-    Communicates with the gripper directly, via socket with string commands, leveraging string names for variables.
-    """
+    """Communicates with the gripper directly via socket with string commands."""
 
     __SET_COMMAND = 'SET'
     __GET_COMMAND = 'GET'
     __ACK_MESSAGE = 'ack'
 
-    # WRITE VARIABLES (CAN ALSO READ)
-    ACT = 'ACT'  # act : activate (1 while activated, can be reset to clear fault status)
-    GTO = 'GTO'  # gto : go to (will perform go to with the actions set in pos, for, spe)
-    ATR = 'ATR'  # atr : auto-release (emergency slow move)
-    ADR = 'ADR'  # adr : auto-release direction (open(1) or close(0) during auto-release)
-    FOR = 'FOR'  # for : force (0-255)
-    SPE = 'SPE'  # spe : speed (0-255)
-    POS = 'POS'  # pos : position (0-255), 0 = open
-    # READ VARIABLES
-    STA = 'STA'  # status (0 = is reset, 1 = activating, 3 = active)
-    PRE = 'PRE'  # position request (echo of last commanded position)
-    OBJ = 'OBJ'  # object detection (0 = moving, 1 = outer grip, 2 = inner grip, 3 = no object at rest)
-    FLT = 'FLT'  # fault (0=ok, see manual for errors if not zero)
+    ACT = 'ACT'
+    GTO = 'GTO'
+    ATR = 'ATR'
+    ADR = 'ADR'
+    FOR = 'FOR'
+    SPE = 'SPE'
+    POS = 'POS'
+    STA = 'STA'
+    PRE = 'PRE'
+    OBJ = 'OBJ'
+    FLT = 'FLT'
 
-    ENCODING = 'UTF-8'  # ASCII and UTF-8 both seem to work
+    ENCODING = 'UTF-8'
 
     def __init__(self):
-        """Constructor."""
         self.socket: typing.Optional[socket.socket] = None
         self.socket_lock = threading.Lock()
         self._min_position: int = 0
@@ -217,7 +207,8 @@ class Robotiq2f85SocketAdapter:
 
     def connect(self, hostname: str, port: int, socket_timeout: float = 2.0) -> None:
         """
-        Connects to the Robotiq2f85 gripper at the given address.
+        Connect to the Robotiq2f85 gripper at the given address.
+
         :param hostname: Hostname or ip.
         :param port: Port.
         :param socket_timeout: Timeout for blocking socket operations.
@@ -228,19 +219,19 @@ class Robotiq2f85SocketAdapter:
             self.socket.settimeout(socket_timeout)
 
     def disconnect(self) -> None:
-        """Closes the connection with the gripper."""
+        """Close the connection with the gripper."""
         with self.socket_lock:
             self.socket.close()
 
     def __set_gripper_variables(self, variable_dict: typing.OrderedDict[str, Union[int, float]]):
         """
-        Sets the variables specified in the dict on the gripper via the socket connection.
+        Set the variables specified in the dict on the gripper via the socket connection.
+
         This command waits for the 'ack' response form the gripper.
 
         :param variable_dict: Dictionary of variables to set (variable_name, value).
         :return: True on successful reception of ack, false if no ack was received
         """
-
         cmd = f'{self.__SET_COMMAND}'
         for variable, value in variable_dict.items():
             cmd += f" {variable} {str(value)}"
@@ -253,18 +244,21 @@ class Robotiq2f85SocketAdapter:
 
     def __set_gripper_variable(self, variable: str, value: Union[int, float]):
         """
-        Sets the specified variable on the gripper via the socket connection.
+        Set the specified variable on the gripper via the socket connection.
+
         This command waits for the 'ack' response form the gripper.
 
         :param variable: Variable to set.
         :param value: Value to set for the variable.
+
         :return: True on successful reception of ack, false if no ack was received
         """
         return self.__set_gripper_variables(OrderedDict([(variable, value)]))
 
     def __get_gripper_variable(self, variable: str):
         """
-        Gets the specified variable value from the gripper using the socket connection.
+        Get the specified variable value from the gripper using the socket connection.
+
         This command blocks until the response has been received or a timeout occurs.
 
         :param variable: Name of the variable to retrieve.
@@ -278,13 +272,17 @@ class Robotiq2f85SocketAdapter:
 
         var_name, value_str = data.decode(self.ENCODING).split()
         if var_name != variable:
-            raise ValueError(f"Unexpected response {data} ({data.decode(self.ENCODING)}): does not match '{variable}'")
+            raise ValueError(
+                f"Unexpected response {data} ({data.decode(self.ENCODING)}): "
+                f"does not match '{variable}'"
+                )
         value = int(value_str)
         return value
 
     def _is_ack(self, data: bytes) -> bool:
         """
-        Checks if received data is an acknowledgement message.
+        Check if received data is an acknowledgement message.
+
         :param data: bytes data received from the socket.
 
         :return if the data is an acknowledgement message.
@@ -292,28 +290,38 @@ class Robotiq2f85SocketAdapter:
         return data.decode(self.ENCODING) == self.__ACK_MESSAGE
 
     def _reset(self):
-        """
-        Reset the gripper.
-        """
+        """Reset the gripper."""
         self.__set_gripper_variable(self.ACT, 0)
         self.__set_gripper_variable(self.ATR, 0)
-        while self.__get_gripper_variable(self.ACT) != 0 or self.__get_gripper_variable(self.STA) != 0:
+        while (
+            self.__get_gripper_variable(self.ACT) != 0 or
+            self.__get_gripper_variable(self.STA) != 0
+        ):
             self.__set_gripper_variable(self.ACT, 0)
             self.__set_gripper_variable(self.ATR, 0)
         time.sleep(0.5)
 
     def activate(self, auto_calibrate: bool = True):
-        """Resets the activation flag in the gripper, and sets it back to one, clearing previous fault flags.
-        :param auto_calibrate: Whether to calibrate the minimum and maximum positions based on actual motion.
+        """
+        Reset the activation flag in the gripper and clear previous fault flags.
+
+        :param auto_calibrate: Whether to calibrate the
+        minimum and maximum positions based on actual motion.
         """
         if not self.is_active:
             self._reset()
-            while self.__get_gripper_variable(self.ACT) != 0 or self.__get_gripper_variable(self.STA) != 0:
+            while (
+                self.__get_gripper_variable(self.ACT) != 0 or
+                self.__get_gripper_variable(self.STA) != 0
+            ):
                 time.sleep(0.01)
 
             self.__set_gripper_variable(self.ACT, 1)
             time.sleep(1.0)
-            while self.__get_gripper_variable(self.ACT) != 1 or self.__get_gripper_variable(self.STA) != 3:
+            while(
+                self.__get_gripper_variable(self.ACT) != 1 or
+                self.__get_gripper_variable(self.STA) != 3
+            ):
                 time.sleep(0.01)
 
         if auto_calibrate:
@@ -321,67 +329,71 @@ class Robotiq2f85SocketAdapter:
 
     @property
     def is_active(self):
-        """Returns whether the gripper is active."""
+        """Return whether the gripper is active."""
         status = self.__get_gripper_variable(self.STA)
         return GripperStatus(status) == GripperStatus.ACTIVE
 
     @property
     def max_force(self):
-        """Returns the maximum force the gripper can provide"""
+        """Return the maximum force the gripper can provide."""
         return self._max_force
 
     @property
     def min_force(self):
-        """Returns the minimum force the gripper can provide"""
+        """Return the minimum force the gripper can provide."""
         return self._min_force
 
     @property
     def max_speed(self):
-        """Returns the maximum force the gripper can move with"""
+        """Return the maximum force the gripper can move with."""
         return self._max_speed
 
     @property
     def min_speed(self):
-        """Returns the minimum force the gripper can move with"""
+        """Return the minimum force the gripper can move with."""
         return self._min_speed
 
     @property
     def min_position(self) -> int:
-        """Returns the minimum position the gripper can reach (open position)."""
+        """Return the minimum position the gripper can reach (open position)."""
         return self._min_position
 
     @property
     def max_position(self) -> int:
-        """Returns the maximum position the gripper can reach (closed position)."""
+        """Return the maximum position the gripper can reach (closed position)."""
         return self._max_position
 
     @property
     def open_position(self) -> int:
-        """Returns what is considered the open position for gripper (minimum position value)."""
+        """Return what is considered the open position for gripper (minimum position value)."""
         return self.min_position
 
     @property
     def closed_position(self) -> int:
-        """Returns what is considered the closed position for gripper (maximum position value)."""
+        """Return what is considered the closed position for gripper (maximum position value)."""
         return self.max_position
 
     @property
     def is_open(self):
-        """Returns whether the current position is considered as being fully open."""
+        """Return whether the current position is considered as being fully open."""
         return self.current_position <= self.open_position
 
     @property
     def is_closed(self):
-        """Returns whether the current position is considered as being fully closed."""
+        """Return whether the current position is considered as being fully closed."""
         return self.current_position >= self.closed_position
 
     @property
     def current_position(self) -> int:
-        """Returns the current position as returned by the physical hardware."""
+        """Return the current position as returned by the physical hardware."""
         return self.__get_gripper_variable(self.POS)
 
     def auto_calibrate(self, log: bool = True) -> None:
-        """Attempts to calibrate the open and closed positions, by slowly closing and opening the gripper.
+        """
+        Attempt to calibrate the open and closed positions.
+
+        This slowly closes and opens the gripper.
+
         :param log: Whether to print the results to log.
         """
         # first try to open in case we are holding an object
@@ -407,31 +419,47 @@ class Robotiq2f85SocketAdapter:
             print(f"Gripper auto-calibrated to [{self.min_position}, {self.max_position}]")
 
     def move(self, position: int, speed: int, force: int) -> Tuple[bool, int]:
-        """Sends commands to start moving towards the given position, with the specified speed and force.
+        """
+        Send commands to start moving towards the given position.
+
         :param position: Position to move to [min_position, max_position]
         :param speed: Speed to move at [min_speed, max_speed]
         :param force: Force to use [min_force, max_force]
-        :return: A tuple with a bool indicating whether the action it was successfully sent, and an integer with
-        the actual position that was requested, after being adjusted to the min/max calibrated range.
-        """
 
+        :return: A tuple with a bool indicating whether the action it was successfully sent,
+        and an integer with the actual position that was requested,
+        after being adjusted to the min/max calibrated range.
+        """
         clip_pos = clip_val(self._min_position, position, self._max_position)
         clip_spe = clip_val(self._min_speed, speed, self._max_speed)
         clip_for = clip_val(self._min_force, force, self._max_force)
 
         # moves to the given position with the given speed and force
-        var_dict = OrderedDict([(self.POS, clip_pos), (self.SPE, clip_spe), (self.FOR, clip_for), (self.GTO, 1)])
+        var_dict = OrderedDict(
+            [
+                (self.POS, clip_pos),
+                (self.SPE, clip_spe),
+                (self.FOR, clip_for),
+                (self.GTO, 1)
+            ]
+        )
         return self.__set_gripper_variables(var_dict), clip_pos
 
-    def move_and_wait_for_pos(self, position: int, speed: int, force: int) -> Tuple[int, ObjectStatus]:  # noqa
-        """Sends commands to start moving towards the given position, with the specified speed and force, and
-        then waits for the move to complete.
+    def move_and_wait_for_pos(
+        self,
+        position: int,
+        speed: int,
+        force: int
+    ) -> Tuple[int, ObjectStatus]:
+        """
+        Send commands to start moving towards the given position waiting for the move to complete.
+
         :param position: Position to move to [min_position, max_position]
         :param speed: Speed to move at [min_speed, max_speed]
         :param force: Force to use [min_force, max_force]
-        :return: A tuple with an integer representing the last position returned by the gripper after it notified
-        that the move had completed, a status indicating how the move ended (see ObjectStatus enum for details). Note
-        that it is possible that the position was not reached, if an object was detected during motion.
+
+        :return: A tuple with an integer representing the last position returned by the gripper
+        after it notified that the move had completed, a status indicating how the move ended.
         """
         set_ok, cmd_pos = self.move(position, speed, force)
         if not set_ok:
@@ -454,7 +482,7 @@ class Robotiq2f85SocketAdapter:
 
 def main(args=None):
     """
-    Main method for the Robotiq2f85AdapterNode standalone node.
+    Run the main method for the Robotiq2f85AdapterNode standalone node.
 
     :param: Arguments passed to the rclpy init call.
     """
